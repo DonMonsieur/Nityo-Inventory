5a0ce978c213d039b29923d85048a88b3c2faa29
Monsieur <monsieurphenomenal@gmail.com>
2023-08-26 07:05:25 +0800

    #4 add seeders

--- backend/database/seeders/InventorySeeder.php
+++ backend/database/seeders/InventorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Inventory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -11,12 +12,23 @@ class InventorySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        DB::table('inventories')->insert([
-            ['product_name' => 'Kilogram', 'unit' => 'kg', 'price' => 1, 'date_of_expiry' => '2023-12-31', 'available_inventory' => 100, 'image' => 'kgsad'],
-            ['product_name' => 'Gram', 'unit' => 'g', 'price' => 2, 'date_of_expiry' => '2023-12-31', 'available_inventory' => 200, 'image' => 'gasd'],
-            ['product_name' => 'Pieces', 'unit' => 'pcs', 'price' => 3, 'date_of_expiry' => '2023-12-31', 'available_inventory' => 50, 'image' => 'pcsasd'],
-        ]);
+        for ($i = 0; $i < 100; $i++) {
+            $product_name = 'Product ' . ($i + 1);
+            
+            while (Inventory::where('product_name', $product_name)->exists()) {
+                $product_name = 'Product ' . ($i + 1) . '-' . rand(1, 1000);
+            }
+
+            Inventory::create([
+                'product_name' => $product_name,
+                'unit' => 'kg',
+                'price' => 1,
+                'date_of_expiry' => '2023-12-31',
+                'available_inventory' => 100,
+                'image' => 'kgsad',
+            ]);
+        }
     }
 }
